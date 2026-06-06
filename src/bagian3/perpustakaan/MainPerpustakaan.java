a586f641555acc2de9fecf517588a8ece8bead89
XenjiZ <165390026+XenjiZ@users.noreply.github.com>
2026-06-06 18:23:03 +0800

    Selesai Bagian 3: Multi-Class

---
+++ src/bagian3/perpustakaan/MainPerpustakaan.java
@@ -0,0 +1,25 @@
+package bagian3.perpustakaan;
+public class MainPerpustakaan {
+    public static void main(String[] args) {
+        Perpustakaan perpus = new Perpustakaan();
+
+        perpus.tambahBuku(new Buku("Laskar Pelangi", "Andrea Hirata", 2005));
+        perpus.tambahBuku(new Buku("Bumi Manusia", "Pramoedya", 1980));
+        perpus.tambahBuku(new Buku("Negeri 5 Menara", "Ahmad Fuadi", 2009));
+
+        perpus.tampilkanKoleksi();
+        System.out.println();
+
+        perpus.pinjamBuku("Bumi Manusia");
+        System.out.println();
+
+        perpus.tampilkanKoleksi();
+        System.out.println();
+
+        perpus.kembalikanBuku("Bumi Manusia");
+        System.out.println();
+
+        System.out.println("Buku karya Ahmad Fuadi:");
+        perpus.cariPenulis("Ahmad Fuadi");
+    }
+}
